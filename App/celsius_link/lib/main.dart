db97858c4e804436e79349a3f366c0ab207d334c
Manz2 <erikmanzer21@gmail.com>
2023-07-30 18:40:21 +0200

    new main

--- App/celsius_link/lib/main.dart
+++ App/celsius_link/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:celsius_link/screens/home/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -10,11 +12,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'CelsiusLink',
+      home: HomeView(),
     );
   }
 }
