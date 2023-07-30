f289b5919f6a927fc591e165b8a10a7f6476e22c
Manz2 <erikmanzer21@gmail.com>
2023-07-30 12:21:29 +0200

    gitignore and secrets

--- Arduino/BME280SensorFirebase/BME280SensorFirebase.ino
+++ Arduino/BME280SensorFirebase/BME280SensorFirebase.ino
@@ -20,18 +20,7 @@
 #include "addons/RTDBHelper.h"
 
 // Insert your network credentials
-#define WIFI_SSID "REPLACE_WITH_YOUR_SSID"
-#define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"
-
-// Insert Firebase project API Key
-#define API_KEY "REPLACE_WITH_YOUR_PROJECT_API_KEY"
-
-// Insert Authorized Email and Corresponding Password
-#define USER_EMAIL "REPLACE_WITH_THE_USER_EMAIL"
-#define USER_PASSWORD "REPLACE_WITH_THE_USER_PASSWORD"
-
-// Insert RTDB URLefine the RTDB URL
-#define DATABASE_URL "REPLACE_WITH_YOUR_DATABASE_URL"
+#include "secrets.h"
 
 // Define Firebase objects
 FirebaseData fbdo;
